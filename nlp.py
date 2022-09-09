cd7b29fc477006ef7e7fa5b24f171a56eb6d58de
Trina <92634994+trinayau@users.noreply.github.com>
2022-09-09 09:58:59 +0100

    add spacy

--- nlp.py
+++ nlp.py
@@ -1,1 +1,12 @@
-# NLP logic
+# NLP logic with spaCy
+
+import spacy
+nlp = spacy.load("en_core_web_md")
+# creating spacy object
+s1=nlp("Will it rain today?")
+s2 =nlp("Is it going to rain today?")
+similarity = s1.similarity(s2) #spacy how similar are these statements
+print(similarity) #tells u how similar it is
+
+#
+
